8b8103ba253e321e2e27554b7f685c0d96e9db5a
thomasstummer <ic15b079@technikum-wien.at>
2016-11-28 20:46:36 +0100

    Added server.c

---
+++ server.c
@@ -0,0 +1,15 @@
+/*
+ * server.c
+ *
+ *  Created on: 28.11.2016
+ *      Author: thomas
+ */
+
+
+#include <stdio.h>
+
+// Test Source File
+int main(int argc, char * argv[])
+{
+	printf("Hi I'm a server!");
+}
