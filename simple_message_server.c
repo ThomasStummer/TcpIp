85ec66cfff32dc786f6729d381942117fa33bd5a
thomasstummer <ic15b079@technikum-wien.at>
2016-12-08 16:30:24 +0100

    Completed and functional server

--- simple_message_server.c
+++ simple_message_server.c
@@ -5,7 +5,7 @@
  *      Author: thomas
  */
 
-
+// Includes
 #include <stdio.h>
 #include <errno.h>
 #include <stdbool.h>
@@ -16,34 +16,58 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <signal.h>
 
-
+// Defines
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
 #define BACKLOG 10
 #define SERVER_LOGIC_PATH "/usr/local/bin/simple_message_server_logic"
 #define SERVER_LOGIC_FILE "simple_message_server_logic"
 
+// Global variables
 const char * programName;
 const char * usageText = 	"usage: simple_message_server options\n"
 							"options:\n"
 							"\t-p, --port <port>	port of the server [0..65535]\n"
 							"\t-h, --help\n";
 
+// Prototypes
 void PrintError(char * funcName, bool evalErrno, const char * message);
-void SpecifyAddrInfo(struct addrinfo * hints);
-// Adopted from file simple_message_client_commandline_handling.c from (R) Prof.
-int smc_parsecommandline(
-    int argc,
-    const char * const argv[],
-    const char **port
-    );
+int ParseCommandLine(int argc, const char * const argv[], const char **port);
+int CreateSignalHandler(void);
+void SignalHandler(int signal);
+int SpecifyAddrInfo(struct addrinfo * hints);
+int CloseSocketDescriptor(int socketDescriptor);
+int CreateAndBindListeningSocket(const char * port, int * socketDescriptor, struct addrinfo ** addrInfoResultsPtr);
+int AcceptIncomingConnections(int socketDescriptor , struct addrinfo ** addrInfoResultsPtr);
+int Spawn(int socketDescriptor, int acceptedSocketDescriptor);
 
-int smc_parsecommandline(
-    int argc,
-    const char * const argv[],
-    const char **port
-    )
+// Functions
+
+void PrintError(char * funcName, bool evalErrno, const char * message)
+{
+	fprintf(stderr, "Error in program ");
+	fprintf(stderr, programName);
+	fprintf(stderr, ": function ");
+	fprintf(stderr, funcName);
+
+	if(message != NULL)
+	{
+		fprintf(stderr, ": ");
+		fprintf(stderr, message);
+	}
+
+	if(evalErrno)
+	{
+		fprintf(stderr, ": ");
+		fprintf(stderr, strerror(errno));
+	}
+
+	fprintf(stderr, "\n");
+}
+
+int ParseCommandLine(int argc, const char * const argv[], const char **port)
 {
     int c;
 
@@ -74,7 +98,7 @@ int smc_parsecommandline(
 
             case 'h':
             	fprintf(stdout, usageText);
-            	return EXIT_SUCCESS;
+            	return EXIT_FAILURE;
                 break;
 
             case '?':
@@ -90,264 +114,256 @@ int smc_parsecommandline(
     	fprintf(stderr, usageText);
     	return EXIT_FAILURE;
     }
+    return EXIT_SUCCESS;
 }
 
-void PrintError(char * funcName, bool evalErrno, const char * message)
+int CreateSignalHandler()
 {
-	fprintf(stderr, "Error in program ");
-	fprintf(stderr, programName);
-	fprintf(stderr, ": function ");
-	fprintf(stderr, funcName);
+	struct sigaction signalAction;
 
-	if(message != NULL)
+	signalAction.sa_handler = SignalHandler;
+	sigemptyset(&signalAction.sa_mask);	// Clear all signals
+	signalAction.sa_flags = SA_RESTART;
+
+	if (sigaction(SIGCHLD, &signalAction, NULL) == -1)
 	{
-		fprintf(stderr, ": ");
-		fprintf(stderr, message);
+		PrintError("CreateSignalHandler() -> sigaction()", true, NULL);
+		return EXIT_FAILURE;
 	}
-
-	if(evalErrno)
-	{
-		fprintf(stderr, ": ");
-		fprintf(stderr, strerror(errno));
-	}
-
-	fprintf(stderr, "\n");
+	return EXIT_SUCCESS;
 }
 
-void SpecifyAddrInfo(struct addrinfo * hints)
+void SignalHandler(int signal)
+{
+	while (waitpid(-1, NULL, WNOHANG) > 0);
+	// -1 ... any child process
+	// NULL ... we don't need detailed information from waitpid
+	// WNNOHANG ... waitpid returns immediately if no child has exited
+}
+
+int SpecifyAddrInfo(struct addrinfo * hints)
 {
 	memset(hints, 0, sizeof(struct addrinfo));
 	hints->ai_family = AF_INET;    		/* IPv4 */
 	hints->ai_socktype = SOCK_STREAM;	/* TCP */
 	hints->ai_flags = AI_PASSIVE;   	/* Server is passive */
+	return EXIT_SUCCESS;
 }
 
-int CloseConnection(int socketDescriptor)
+int CloseSocketDescriptor(int socketDescriptor)
 {
 	if(socketDescriptor != -1)
 	{
 		if(close(socketDescriptor) == -1)
 		{
-			PrintError("main() -> close()", true, NULL);
+			PrintError("CloseSocketDescriptor() -> close()", true, NULL);
 			return EXIT_FAILURE;
 		}
 		return EXIT_SUCCESS;
 	}
-	else
-	{
-		return EXIT_FAILURE;
-	}
+	return EXIT_FAILURE;
 }
 
-// Test Source File
-int main(int argc, const char * const argv[])
+int CreateAndBindListeningSocket(const char * port, int * socketDescriptor, struct addrinfo ** addrInfoResultsPtr)
 {
 	struct addrinfo addrInfoSettings;
-	struct addrinfo * addrInfoResults, * addrInfoResultsPtr;
-	int socketDescriptor = 0, acceptedSocketDescriptor = 0;
-	const struct sockaddr sockAddr;
-	const char * port;
+	struct addrinfo * addrInfoResults;
 	int r=0;
-	pid_t pid = -1;
-	int status = 0;
-	pid_t wait_pid = -1;
-	int i=0;
 
-	programName = argv[0];
-
-	// ParseArguments
-	if(smc_parsecommandline(argc, argv, &port) == EXIT_FAILURE)
+	if(SpecifyAddrInfo(&addrInfoSettings) == EXIT_FAILURE)
 	{
-		return EXIT_FAILURE; 	//  Fallunterscheidung zw. failure und ok return der Func fehlt!!
+		PrintError("CreateAndBindSocket() -> SpecifyAddrInfo()", true, NULL);
+		return EXIT_FAILURE;
 	}
-
-
-	// Open passive connection
-
-	SpecifyAddrInfo(&addrInfoSettings);
-
 
 	r = getaddrinfo(NULL, port, &addrInfoSettings, &addrInfoResults);
 	if(r != 0)
 	{
-		PrintError("main() -> getaddrinfo()", false, gai_strerror(r));
+		PrintError("CreateAndBindSocket() -> getaddrinfo()", false, gai_strerror(r));
 		return EXIT_FAILURE;
 	}
 
-	for(addrInfoResultsPtr = addrInfoResults;
-		addrInfoResultsPtr != NULL;
-		addrInfoResultsPtr = addrInfoResultsPtr->ai_next)
+	for(*addrInfoResultsPtr = addrInfoResults;
+		*addrInfoResultsPtr != NULL;
+		*addrInfoResultsPtr = (*addrInfoResultsPtr)->ai_next)
 	{
-
-
-		socketDescriptor = socket(addrInfoResultsPtr->ai_family,
-									addrInfoResultsPtr->ai_socktype,
-									addrInfoResultsPtr->ai_protocol);
-		if(socketDescriptor == -1)
+		*socketDescriptor = socket((*addrInfoResultsPtr)->ai_family,
+									(*addrInfoResultsPtr)->ai_socktype,
+									(*addrInfoResultsPtr)->ai_protocol);
+		if(*socketDescriptor == -1)
 		{
 			continue;	// Try next
 		}
 
-		if(bind(socketDescriptor, addrInfoResultsPtr->ai_addr, addrInfoResultsPtr->ai_addrlen) == 0)
+		if(bind(*socketDescriptor,
+				(*addrInfoResultsPtr)->ai_addr,
+				(*addrInfoResultsPtr)->ai_addrlen) == 0)
 		{
 			break;	// Success
 		}
 
 		// bind() failed
-		if(CloseConnection(socketDescriptor) == EXIT_FAILURE)
+		if(CloseSocketDescriptor(*socketDescriptor) == EXIT_FAILURE)
 		{
-			PrintError("main() -> CloseConnection()", true, NULL);
+			PrintError("CreateAndBindSocket() -> CloseSocketDescriptor()", true, NULL);
 			return EXIT_FAILURE;
 		}
 	}
 
-	if(addrInfoResultsPtr == NULL)
+	if(*addrInfoResultsPtr == NULL)
 	{
 		// Wasn't able to bind any address
-		PrintError("main()", true, "Could not bind to any address");
+		PrintError("CreateAndBindSocket()", true, "Could not bind to any address");
 		return EXIT_FAILURE;
 	}
 
 	freeaddrinfo(addrInfoResults);
+	addrInfoResults = NULL;
 
-
-	if(listen(socketDescriptor, BACKLOG) == -1)
+	if(listen(*socketDescriptor, BACKLOG) == -1)
 	{
-		PrintError("main() -> listen()", true, NULL);
+		PrintError("CreateAndBindSocket() -> listen()", true, NULL);
 		return EXIT_FAILURE;
 	}
 
-	// accept in schleife
-//	for(;;)
+	return EXIT_SUCCESS;
+}
+
+int AcceptIncomingConnections(int socketDescriptor , struct addrinfo ** addrInfoResultsPtr)
+{
+	int acceptedSocketDescriptor = 0;
+
+	for(;;)
 	{
-		acceptedSocketDescriptor = accept(socketDescriptor, addrInfoResultsPtr->ai_addr, &(addrInfoResultsPtr->ai_addrlen));
+		// Accept incoming connection
+		acceptedSocketDescriptor = accept(socketDescriptor, (*addrInfoResultsPtr)->ai_addr, &((*addrInfoResultsPtr)->ai_addrlen));
 		if(acceptedSocketDescriptor == -1)
 		{
-			PrintError("main() -> accept()", true, NULL);
+			PrintError("AcceptIncomingConnections() -> accept()", true, NULL);
 			return EXIT_FAILURE;
 		}
 
-
-
-		// spawn()
-		pid = fork();
-
-		printf("\nAfter fork\n"); 	// DELETE THIS DEBUGGING LINE
-
-		if(pid == -1)
+		// Fork new process and execute business logic
+		if(Spawn(socketDescriptor, acceptedSocketDescriptor) == EXIT_FAILURE)
 		{
-			PrintError("main() -> fork()", true, NULL);
-			CloseConnection(socketDescriptor);
-			// close acc.s.desc?
-			return EXIT_FAILURE;
-		}
-
-		if(pid == 0) 	// Child process
-		{
-			printf("\nI am child\n"); 	// DELETE THIS DEBUGGING LINE
-			// Close unneeded descriptor
-		/*	if (close(socketDescriptor) == -1)
-			{
-				PrintError("Child process: main() -> close()", true, NULL);
-				//CloseConnection(socketDescriptor);
-				printf("\nChild: close(socketDesc. failed\n"); 	// DELETE THIS DEBUGGING LINE	_Exit(EXIT_FAILURE);
-			}
-*/
-			// Redirect stdin
-			if (dup2(acceptedSocketDescriptor, STDIN_FILENO) == -1)
-			{
-				PrintError("Child process: main() -> dup2()", true, NULL);
-				//CloseConnection(socketDescriptor);
-				fprintf(stderr,"\nChild: dup2 -1 failed\n"); 	// DELETE THIS DEBUGGING LINE _Exit(EXIT_FAILURE);
-			}
-
-			// Redirect stdout
-			if (dup2(acceptedSocketDescriptor, STDOUT_FILENO) == -1)
-			{
-				PrintError("Child process: main() -> dup2()", true, NULL);
-				//CloseConnection(socketDescriptor);
-				close(STDIN_FILENO); // return Wert checken!
-				fprintf(stderr,"\nChild: dup2 -2 failed\n"); 	// DELETE THIS DEBUGGING LINE      _Exit(EXIT_FAILURE);
-			}
-			fprintf(stderr, "\nChild redirected stdio\n"); 	// DELETE THIS DEBUGGING LINE
-			// Close unneeded descriptor
-/*			if (close(acceptedSocketDescriptor) == -1)
-			{
-				PrintError("Child process: main() -> dup2()", true, NULL);
-				//CloseConnection(socketDescriptor);
-				close(STDIN_FILENO); // return Wert checken!
-				close(STDOUT_FILENO); // return Wert checken!
-				_Exit(EXIT_FAILURE);
-			}
-*/
-			// Execute server logic program
-			execl(SERVER_LOGIC_PATH, SERVER_LOGIC_FILE, (char *) NULL);
-			// Child process is not allowed to reach this point
-			PrintError("main()", true, "Child process reached unallowed source region");
-			_Exit(EXIT_FAILURE);
-
-			printf("\n%d\n", ++i); 	// DELETE THIS DEBUGGING LINE
-		}
-
-		// No error and not child process -> parent process
-
-		printf("\nI am parent\n"); 	// DELETE THIS DEBUGGING LINE
-
-/*
-		// Close unneeded descriptor
-			if (close(acceptedSocketDescriptor) == -1)
-			{
-				PrintError("main() -> close()", true, NULL);
-				return EXIT_FAILURE;
-			}
-*/
-	}
-
-	// Wait for child process to prevent it from becoming a zombie process
-	while((wait_pid = waitpid(pid, &status, 0)) != pid)
-	{
-		if (wait_pid == -1)
-		{
-			/* eintr: unterbrechung -> nochmal probieren */
-			if (errno == EINTR)
-			{
-				continue;
-			}
-			/* wenn nicht errno EINTR ist, dann passt etwas nicht und es wird errno gesetzt und auch pid und openfile zurueck */
-			errno = ECHILD;
-			pid = -1;
+			PrintError("AcceptIncomingConnections() -> Spawn()", false, NULL);
 			return EXIT_FAILURE;
 		}
 	}
+	return EXIT_SUCCESS;
+}
 
-	printf("\nParent: Child terminated\n"); 	// DELETE THIS DEBUGGING LINE
+int Spawn(int socketDescriptor, int acceptedSocketDescriptor)
+{
+	pid_t pid = -1;
+	pid = fork();
 
-	/* schaut sich hier an, "wie" beendet worden ist, ob ordnungsgemaess oder nicht */
-	if (WIFEXITED(status))
+	if(pid == -1)
 	{
-		pid = -1;
-		//return WEXITSTATUS(status);
-	}
-	else
-	{
-		errno = ECHILD;
-		pid = -1;
+		PrintError("main() -> fork()", true, NULL);
+		CloseSocketDescriptor(acceptedSocketDescriptor);
+		CloseSocketDescriptor(socketDescriptor);
 		return EXIT_FAILURE;
 	}
 
-	//test close here
-	// Close unneeded descriptor
-				if (close(acceptedSocketDescriptor) == -1)
-				{
-					PrintError("main() -> close()", true, NULL);
-					return EXIT_FAILURE;
-				}
+	if(pid == 0) 	// Child process
+	{
+		// Close unneeded descriptor
+		if (CloseSocketDescriptor(socketDescriptor) == EXIT_FAILURE)
+		{
+			PrintError("Child process: Spawn() -> CloseSocketDescriptor(socketDescriptor)", true, NULL);
+			CloseSocketDescriptor(acceptedSocketDescriptor);
+			_Exit(EXIT_FAILURE);
+		}
 
-	if(CloseConnection(socketDescriptor) == EXIT_FAILURE)
-				{
-					return EXIT_FAILURE;
-				}
+		// Redirect stdin
+		if (dup2(acceptedSocketDescriptor, STDIN_FILENO) == -1)
+		{
+			PrintError("Child process: Spawn() -> dup2()", true, NULL);
+			CloseSocketDescriptor(acceptedSocketDescriptor);
+			CloseSocketDescriptor(socketDescriptor);
+			_Exit(EXIT_FAILURE);
+		}
 
-	printf("ServerFunc main() exits successfully now\n");
+		// Redirect stdout
+		if (dup2(acceptedSocketDescriptor, STDOUT_FILENO) == -1)
+		{
+			PrintError("Child process: Spawn() -> dup2()", true, NULL);
+			CloseSocketDescriptor(acceptedSocketDescriptor);
+			CloseSocketDescriptor(socketDescriptor);
+			close(STDIN_FILENO);
+			_Exit(EXIT_FAILURE);
+		}
+
+		// Close unneeded descriptor
+		if (CloseSocketDescriptor(acceptedSocketDescriptor) == EXIT_FAILURE)
+		{
+			PrintError("Child process: Spawn() -> CloseSocketDescriptor(acceptedSocketDescriptor)", true, NULL);
+			CloseSocketDescriptor(socketDescriptor);
+			close(STDIN_FILENO);
+			close(STDOUT_FILENO);
+			_Exit(EXIT_FAILURE);
+		}
+
+		// Execute server logic program
+		execl(SERVER_LOGIC_PATH, SERVER_LOGIC_FILE, (char *) NULL);
+
+		// Child process is not allowed to reach this point
+		PrintError("Spawn()", true, "Child process reached unallowed code region");
+		_Exit(EXIT_FAILURE);
+	}
+
+	// No error and not child process -> parent process -> close unneeded descriptor
+	if (close(acceptedSocketDescriptor) == -1)
+	{
+		PrintError("main() -> close()", true, NULL);
+		return EXIT_FAILURE;
+	}
+}
+
+int main(int argc, const char * const argv[])
+{
+	struct addrinfo * addrInfoResultsPtr;
+	int socketDescriptor = 0;
+	const struct sockaddr sockAddr;
+	const char * port;
+
+	programName = argv[0];
+
+	// ParseArguments
+	if(ParseCommandLine(argc, argv, &port) == EXIT_FAILURE)
+	{
+		PrintError("main() -> ParseCommandLine()", false, NULL);
+		return EXIT_FAILURE;
+	}
+
+	// Create signal handler to prevent child processes from becoming zombie processes
+	if(CreateSignalHandler() == EXIT_FAILURE)
+	{
+		PrintError("main() -> CreateSignalHandler()", false, NULL);
+		return EXIT_FAILURE;
+	}
+
+	// Set up and bind socket
+	if(CreateAndBindListeningSocket(port, &socketDescriptor, &addrInfoResultsPtr) == EXIT_FAILURE)
+	{
+		PrintError("main() -> CreateAndBindSocket()", false, NULL);
+		return EXIT_FAILURE;
+	}
+
+	// Accept incoming connections
+	if(AcceptIncomingConnections(socketDescriptor, &addrInfoResultsPtr) == EXIT_FAILURE)
+	{
+		PrintError("main() -> AcceptIncomingConnections()", false, NULL);
+		return EXIT_FAILURE;
+	}
+
+	// Close socket descriptor
+	if(CloseSocketDescriptor(socketDescriptor) == EXIT_FAILURE)
+	{
+		PrintError("main() -> CloseSocketDescriptor(socketDescriptor)", true, NULL);
+		return EXIT_FAILURE;
+	}
+
 	return EXIT_SUCCESS;
 }
