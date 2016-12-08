3098b62c26d65951d16c2fcc5a6e0f318ffcfcf1
pmatula <patrick.matula@gmail.com>
2016-12-08 17:58:24 +0100

    -h implemented

--- simple_message_client.c
+++ simple_message_client.c
@@ -1,26 +1,24 @@
 /*
- * simple_message_client.c
+* hallo
+ * @file simple_message_client.c
+ * @author Thomas Stummer <thomas.stummer@technikum-wien.at>
+ * @author Patrick Matula <patrick.matula@technikum-wien.at>
+ * @version 1.0
  *
- *  VCS - TCP/IP
- *  BIC
- *  3A1
- *
- *  Authors: ic15b008 Matula Patrick
- *    		 ic15b079 Stummer Thomas
+ * Verteilte Systeme - TCP/IP
  */
 
-// Includes
 #include <stdio.h>
-#include <errno.h>
-#include <stdbool.h>
-#include <string.h>
 #include <stdlib.h>
-#include <getopt.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <string.h>
+#include <arpa/inet.h>
 #include <unistd.h>
-#include <signal.h>
+#include <errno.h>
+#include <limits.h>
+#include "simple_message_client_commandline_handling.h"
 
 // Defines
 #define EXIT_SUCCESS 0
@@ -30,48 +28,73 @@
 const char * programName;
 
 // Prototypes
-void PrintError(char * funcName, bool evalErrno, const char * message);
 int CloseSocketDescriptor(int socketDescriptor);
-
-// Functions
+void PrintError(char * funcName, bool evalErrno, const char * message);
+void usagefunc(FILE *outputStream, const char *programName, int exitCode);
 
 void PrintError(char * funcName, bool evalErrno, const char * message)
 {
-	fprintf(stderr, "Error in program ");
-	fprintf(stderr, programName);
-	fprintf(stderr, ": function ");
-	fprintf(stderr, funcName);
+    fprintf(stderr, "Error in program ");
+    fprintf(stderr, programName);
+    fprintf(stderr, ": function ");
+    fprintf(stderr, funcName);
 
-	if(message != NULL)
-	{
-		fprintf(stderr, ": ");
-		fprintf(stderr, message);
-	}
+    if(message != NULL)
+    {
+        fprintf(stderr, ": ");
+        fprintf(stderr, message);
+    }
 
-	if(evalErrno)
-	{
-		fprintf(stderr, ": ");
-		fprintf(stderr, strerror(errno));
-	}
+    if(evalErrno)
+    {
+        fprintf(stderr, ": ");
+        fprintf(stderr, strerror(errno));
+    }
 
-	fprintf(stderr, "\n");
+    fprintf(stderr, "\n");
+}
+
+void usagefunc(FILE *outputStream, const char *programName, int exitCode){
+    fprintf(outputStream, "usage: %s\n", programName);
+    fprintf(outputStream, "options:\n");
+    fprintf(outputStream, "-s, --server \t <server> full qualified domain name or IP address of the server\n");
+    fprintf(outputStream, "-p, --port \t <port> well-known port of the server [0..65535]\n");
+    fprintf(outputStream, "-u, --user \t <name> name of the posting user\n");
+    fprintf(outputStream, "-i, --image \t <URL> URL pointing to an image of the posting user\n");
+    fprintf(outputStream, "-m, --message \t <message> message to be added to the bulletin board\n");
+    fprintf(outputStream, "-v, --verbose \t verbose output\n");
+    fprintf(outputStream, "-h, --help");
+
+    exit(exitCode);
 }
 
 int CloseSocketDescriptor(int socketDescriptor)
 {
-	if(socketDescriptor != -1)
-	{
-		if(close(socketDescriptor) == -1)
-		{
-			PrintError("CloseSocketDescriptor() -> close()", true, NULL);
-			return EXIT_FAILURE;
-		}
-		return EXIT_SUCCESS;
-	}
-	return EXIT_FAILURE;
+    if(socketDescriptor != -1)
+    {
+        if(close(socketDescriptor) == -1)
+        {
+            PrintError("CloseSocketDescriptor() -> close()", true, NULL);
+            return EXIT_FAILURE;
+        }
+        return EXIT_SUCCESS;
+    }
+    return EXIT_FAILURE;
 }
 
-int main(int argc, char * argv[])
-{
-	printf("Hi I'm a client!");
+
+int main(int argc, const char **argv) {
+
+    /* check parameter */
+    const char *server = NULL;
+    const char *port = NULL;
+    const char *user = NULL;
+    const char *message = NULL;
+    const char *img_url = NULL;
+    int verbose = -1;
+
+    smc_parsecommandline(argc, argv, &usagefunc, &server, &port, &user, &message, &img_url, &verbose);
+
+    return 0;
 }
+
