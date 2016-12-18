6b92e6966630843676b665aff8de1fd6b8171137
pmatula <patrick.matula@gmail.com>
2016-12-18 10:48:48 +0100

    added -v mode and fixed a bug with png (untested but I'm pretty sure).

--- simple_message_client.c
+++ simple_message_client.c
@@ -30,13 +30,13 @@
 
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
-#define size 8
 
 /*
  * --------------------------------------------------------------- globals --
  */
 
 const char * programName;
+int verbose = false;
 
 /*
  * ------------------------------------------------------------- prototypes --
@@ -48,6 +48,7 @@ void usagefunc(FILE *outputStream, const char *programName, int exitCode);
 void initSocketAndConnect(const char *server, const char *port, int *sfd);
 void sendMessage(int sfd, const char* user, const char* message, const char* img_url);
 int readResponse(int sfd);
+void verboseOutput(const char* text);
 
 /*
  * ------------------------------------------------------------- functions --
@@ -142,21 +143,24 @@ int CloseSocketDescriptor(int socketDescriptor)
  */
 void initSocketAndConnect(const char *server, const char *port, int *sfd)
 {
+    verboseOutput("function initSocketAndConnect() :: init some addrinfo structs.");
     struct addrinfo hints, *res, *rp;
 
-    /* reset values */
+    verboseOutput("function initSocketAndConnect() :: reseting values for hints.");
     memset(&hints, 0, sizeof(hints));
-    /* allow ipv4 and ipv6 */
+    verboseOutput("function initSocketAndConnect() :: specifying some filters for hints.");
     hints.ai_family = AF_UNSPEC;
     /* only tcp */
     hints.ai_socktype = SOCK_STREAM;
 
+    verboseOutput("function initSocketAndConnect() :: Get all available addresses (possible sockets).");
     if((getaddrinfo(server,port, &hints, &res)) != 0)
     {
         printError("initSocket", true, "Error getaddrinfo()");
     }
 
-    /* copy & paste from man page from getaddrinfo(3) */
+    verboseOutput("function initSocketAndConnect() :: Try to connect to a socket (loop).");
+    /* copy & paste man page from getaddrinfo(3) */
     for (rp = res; rp != NULL; rp = rp->ai_next)
     {
         *sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
@@ -169,6 +173,7 @@ void initSocketAndConnect(const char *server, const char *port, int *sfd)
         close(*sfd);
     }
 
+    verboseOutput("function initSocketAndConnect() :: cleanup with structs.");
     freeaddrinfo(res);
 }
 
@@ -184,46 +189,60 @@ void initSocketAndConnect(const char *server, const char *port, int *sfd)
  */
 void sendMessage(int sfd, const char* user, const char* message, const char* img_url)
 {
+    verboseOutput("function sendMessage() :: duplicate socket descriptor for write access.");
     int sdw = dup(sfd);
     if(sdw == -1)
     {
         printError("sendMessage()", true, "error with dup(sfd)");
         return;
     }
+    verboseOutput("function sendMessage() :: dup was successfully.");
 
+    verboseOutput("function sendMessage() :: try to fdopen with our new sdw(SocketDescirptorWrite).");
     FILE *fpw = fdopen(sdw, "w");
     if (fpw == NULL)
     {
         printError("sendMessage()", true, "fdopen with w doesn't work");
 	    return;
     }
+    verboseOutput("function sendMessage() :: fdopen was successful.");
 
     /* request with image url */
     if (!(img_url == NULL))
     {
+        verboseOutput("function sendMessage() :: You call this program with img_url parameter.");
+        verboseOutput("function sendMessage() :: Try to send message.");
         if(fprintf(fpw, "user=%s\nimg=%s\n%s\n", user, img_url, message) < 0)
         {
             printError("sendMessage()", true, "error fprintf(fpw, \"user=%s\\nimg=%s\\n%s\\n\", user, img_url, message)");
         }
+        verboseOutput("function sendMessage() :: message sent successful.");
     }
     else
     {
+        verboseOutput("function sendMessage() :: You call this program not with img_url parameter.");
+        verboseOutput("function sendMessage() :: Try to send message.");
         if(fprintf(fpw, "user=%s\n%s\n", user, message) < 0)
         {
             printError("sendMessage()", true, "error fprintf(fpw, \"user=%s\\n%s\\n\", user, message)");
         }
+        verboseOutput("function sendMessage() :: message sent successful.");
     }
 
-    /*close write */
+    verboseOutput("function sendMessage() :: fflush fpw.");
     fflush(fpw);
+    verboseOutput("function sendMessage() :: shutdown the sdw.");
     if(shutdown(sdw, SHUT_WR) != 0)
     {
 	    printError("sendMessage()", true, "error shutdown(sdw, SHUT_WR)");
     }
+    verboseOutput("function sendMessage() :: shutdown sdw successful.");
+    verboseOutput("function sendMessage() :: fclose the fpw.");
     if(fclose(fpw) != 0)
     {
 	    printError("sendMessage()", true, "error fclose(fpw)");
     }
+    verboseOutput("function sendMessage() :: fclose fpw successful.");
 }
 
 /**
@@ -237,7 +256,7 @@ void sendMessage(int sfd, const char* user, const char* message, const char* img
  */
 int readResponse(int sfd)
 {
-    /* open read */
+    verboseOutput("Function readResponse() :: Create file pointer with socketdescriptor in read.");
     FILE *fpr = fdopen(sfd, "r");
     if (fpr == NULL)
     {
@@ -254,6 +273,7 @@ int readResponse(int sfd)
     int lengthHtml = 0;
     int read = 0;
 
+    verboseOutput("Function readResponse() :: Trying to read first 3 parameters (status, file, len)");
     /* read first 3 lines */
     for (i = 0; i < 3; i++)
     {
@@ -268,8 +288,9 @@ int readResponse(int sfd)
         	case 0:
 				if(sscanf(line, "status=%d", &status) == EOF)
 				{
-					printError("readResponse()", true, "sscanf() failed");
+					printError("readResponse()", true, "sscanf() for status failed");
 				}
+                verboseOutput("Function readResponse() :: read successfully status.");
 				break;
         	case 1:
 				filenameHtml = (char*)realloc(filenameHtml, read);
@@ -279,18 +300,21 @@ int readResponse(int sfd)
 				}
 				if(sscanf(line, "file=%s", filenameHtml) == EOF)
 				{
-					printError("readResponse()", true, "sscanf() failed");
+					printError("readResponse()", true, "sscanf() for filenameHtml failed");
 				}
-				break;
+                verboseOutput("Function readResponse() :: read successfully filenameHtml.");
+                break;
         	case 2:
 				if(sscanf(line, "len=%d", &lengthHtml) == EOF)
 				{
-					printError("readResponse()", true, "sscanf() failed");
+					printError("readResponse()", true, "sscanf() for lengthHtml failed");
 				}
-				break;
+                verboseOutput("Function readResponse() :: read successfully lengthHtml.");
+                break;
         }
     }
-	/* now we have all 3 parameters for the first response html thing */
+	verboseOutput("Function readResponse() :: 3 parameters read successully. Now I try to read the whole html file.");
+    verboseOutput("Function readResponse() :: Create file pointer to the new HTML file.");
 	FILE *fpResponseHtmlFile = fopen(filenameHtml, "w");
 	if(fpResponseHtmlFile == NULL)
 	{
@@ -299,18 +323,22 @@ int readResponse(int sfd)
 
 	char buffer[lengthHtml];
 
+    verboseOutput("Function readResponse() :: Read from socket for HTML file.");
 	if(fread(buffer, 1, lengthHtml, fpr) != 1)
 	{
 		printError("readResponse()", true, "fread() failed");
 	}
 
+    verboseOutput("Function readResponse() :: Write from buffer to file.");
 	if(fwrite(buffer, lengthHtml, 1, fpResponseHtmlFile) != 1)
 	{
 		printError("readResponse()", true, "fwrite() failed");
 	}
 
-	/* now it is time for the png */
-	for (i = 0; i < 3; i++) {
+    verboseOutput("Function readResponse() :: HTML File completed.");
+    verboseOutput("Function readResponse() :: PNG File starting.");
+    verboseOutput("Function readResponse() :: Try to read 2 parameters (filenamePng and lengthPng).");
+	for (i = 0; i < 2; i++) {
 		read = getline(&line, &len, fpr);
 		if (i == 0)
 		{
@@ -318,22 +346,56 @@ int readResponse(int sfd)
 			if(filenamePng == NULL)
 			{
 					printError("readResponse()", true, "no memory for realloc filenamePng");
+                    return EXIT_FAILURE;
 			}
-			sscanf(line, "file=%s", filenamePng);
+			if(sscanf(line, "file=%s", filenamePng) == EOF)
+            {
+                printError("readResponse()", true, "sscanf() for filenamePng failed");
+                return EXIT_FAILURE;
+            }
+            verboseOutput("Function readResponse() :: read successfully filenamePng.");
 		}
 		if (i == 1)
 		{
-			sscanf(line, "len=%d", &lengthPng);
-		}
+			if(sscanf(line, "len=%d", &lengthPng) == EOF)
+            {
+                printError("readResponse()", true, "sscanf() for lengthPng failed");
+                return EXIT_FAILURE;
+            }
+            verboseOutput("Function readResponse() :: read successfully lengthPng.");
+        }
 	}
 
+    verboseOutput("Function readResponse() :: create File pointer for png file.");
 
-	FILE *fpResponseHtmlPng = fopen(filenamePng, "w");
+    FILE *fpResponseHtmlPng = fopen(filenamePng, "w");
+    if(fpResponseHtmlPng == NULL)
+    {
+        printError("readResponse()", true, "fopen(filenamePng, 'w') failed");
+    }
 	char bufferPng[lengthPng];
-	fread(bufferPng, 1, lengthPng, fpr);
-	fwrite(bufferPng, lengthPng, 1, fpResponseHtmlPng);
 
+    verboseOutput("Function readResponse() :: read from socket to buffer (png).");
+	if(fread(bufferPng, 1, lengthPng, fpr) != 1)
+    {
+        printError("readResponse()", true, "fread() for png failed");
+    }
+    verboseOutput("Function readResponse() :: write from buffer to file (png).");
+    if(fwrite(bufferPng, lengthPng, 1, fpResponseHtmlPng) != 1)
+    {
+        printError("readResponse()", true, "fread() for png failed");
+    }
+
+    verboseOutput("Function readResponse() :: everything done. return with right exit value.");
     return status;
+}
+
+void verboseOutput(const char *text)
+{
+    if(verbose)
+    {
+        fprintf(stdout, "%s\n", text);
+    }
 }
 
 /**
@@ -357,15 +419,21 @@ int main(int argc, const char **argv) {
     const char *user = NULL;
     const char *message = NULL;
     const char *img_url = NULL;
-    int verbose = -1;
+    int verboseParam = -1;
 
-    smc_parsecommandline(argc, argv, &usagefunc, &server, &port, &user, &message, &img_url, &verbose);
+    verboseOutput("Checking parameter...");
+    smc_parsecommandline(argc, argv, &usagefunc, &server, &port, &user, &message, &img_url, &verboseParam);
+    verboseOutput("parameter checking successfully.");
 
-    printError("Debug", false, "Line 2");
+    verbose = verboseParam;
+
+    verboseOutput("Entering function initSocketAndConnect().");
     initSocketAndConnect(server, port, &sfd);
-    printError("Debug", false, "Line 3");
+    verboseOutput("Leaving function initSocketAndConnect().");
+    verboseOutput("Entering function sendMessage().");
     sendMessage(sfd, user, message, img_url);
-    printError("Debug", false, "Line 4");
+    verboseOutput("Leaving function sendMessage().");
+    verboseOutput("Entering function readResponse(). return value of function readResponse() is the return value for main program.");
     return readResponse(sfd);
 }
 
